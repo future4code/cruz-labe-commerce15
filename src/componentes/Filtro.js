007b3019401d02b9b7bca7dbcf2f8bb30a011e5e
Lucas Bacelar <lucasbacela@hotmail.com>
2021-03-17 18:02:56 -0300

    Adição das funções de filtro dos produtos

--- src/componentes/Filtro.js
+++ src/componentes/Filtro.js
@@ -12,18 +12,55 @@ const FiltroContainer = styled.div`
     }
 `
 
-export default class Filtro extends React.Component {
 
+
+export default class Filtro extends React.Component {
+    state = {
+        valorMinimo: 1,
+        valorMaximo: 1000,
+        nomeProduto: '',    
+    }
+
+    onChangeProduto = (event) => {
+        this.setState({nomeProduto: event.target.value})
+
+    }
+
+    onChangeValorMinimo = (event) => {
+        this.setState({valorMinimo:event.target.value});
+
+    }
+
+    onChangeValorMaximo = (event) => {
+        this.setState({valorMaximo: event.target.value})
+    }
+
+    componentDidUpdate() {
+        console.log("nada");
+    }
     render() {
+        
         return (
             <FiltroContainer>
                 <h3>{"Filtros"}</h3>
                 <label>{"Valor minimo:"}</label>
-                <input />
+                <input 
+                    type="number"
+                    value={this.state.valorMinimo}
+                    onChange={this.onChangeValorMinimo}
+                />
                 <label>{"Valor maximo:"}</label>
-                <input />
+                <input 
+                    type="number"
+                    value={this.state.valorMaximo}
+                    onChange={this.onchangeValorMaximo}
+    v            />
                 <label>{"Busca por nome:"}</label>
-                <input />
+                <input 
+                    type="text"
+                    value={this.state.nomeProduto}
+                    onChange={this.onChangeProduto}
+                />
             </FiltroContainer>
         )
     }
